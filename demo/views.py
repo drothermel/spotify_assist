af8befdd2bb285628ec757176d80f97c9d02916b
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-01-02 18:35:19 +0100

    first few steps in course example

---
+++ demo/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+
+def first(request):
+    return HttpResponse("First message from views")
+
+
+class Another(View):
+
+    def get(self, request):
+        return HttpResponse("This is another function inside class")
